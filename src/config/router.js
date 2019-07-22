54a85258e2e7b6d919ecd12526b1a52da2e7fc3e
Nelson Bassey <nelson@Nelsons-MacBook-Pro.local>
2019-07-23 01:54:23 +0200

    up

--- src/config/router.js
+++ src/config/router.js
@@ -1,10 +1,6 @@
 import React, { lazy, Suspense, Component } from 'react';
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import { ScrollContext } from 'react-router-scroll-4';
-import request from 'superagent';
-
-
-import App from '../App';
 import Product from '../pages/Product.js';
 import Upload from '../pages/upload';
 import Blog from '../pages/blog';
@@ -57,76 +53,22 @@ import Careers from '../pages/careers_opportunities';
 import SocialResponsibility from '../pages/social_responsibility';
 import Refer from '../pages/refer_a_friend';
 
+const App = lazy(() => import('../App')); 
 
-class AppRouter extends Component{
 
-    
- 
-    render(){
-        return(
-            <Router onUpdate={() => window.scrollTo(0, 0)} basename="/dev" >
+const outer = () => (
+    <Router onUpdate={() => window.scrollTo(0, 0)}>
         <Switch>
-            <Route exact path={"/company2"} component={Company2} />
-            <Route exact path={"/"} component={ComingSoon1}/>
-            <Route exact path={"/12hdgsikdhlmsdne"} component={App} />
-            <Route exact path={"/upload"} component={Upload} />
-            <Route exact path={"/blog/:nav"} component={Blog} />
-            <Route exact path={"/dev"} component={ComingSoon1} />
-            <Route exact path={"/blog/post/:blog_id/:blog_name"} component={Post} />
-            <Route exact path={"/product/:slug/:number"} component={Product} />
-            <Route exact path={"/wholesale/:slug/:number"} component={Wholesale} />
-            <Route exact path={"/search/:query/category/:category_no"} component={Search} />
-            <Route exact path={"/shop"} component={Search} />
-            <Route exact path={"/contact"} component={Contact} />
-            <Route exact path={"/help"} component={Help} />
-            <Route exact path={"/points"} component={PointOfSale} />
-            <Route exact path={"/info/:infocategory"} component={Info} />
-            {/* <Route exact path={"/m2m"} component={M2M}/> */}
-            <Route exact path={"/size-guide"} component={SizeGuide} />
-            <Route exact path={"/m2m2"} component={M2M2} />
-            <Route exact path={"/gallery"} component={Gallery} />
-            <Route exact path={"/quote"} component={WholesaleQuote} />
-            <Route exact path={"/fabric"} component={Fabric} />
-            <Route exact path={"/fabric_single"} component={Fabric_Single} />
-            <Route exact path={"/fabric_search"} component={Fabric_Display} />
-            <Route exact path={"/fabric/upload"} component={Fabric_Upload} />
-            <Route exact path={"/fabric/create"} component={PreFabricUpload} />
-            <Route exact path={"/fabric/upload/select/:fabric_id"} component={Fabric_Upload_Select} />
-            <Route exact path={"/signin"} component={Signin} />
-            <Route exact path={"/visit_rwanda"} component={visitRwanda} />
-            <Route exact path={"/student_discount"} component={student_discount} />
-            <Route exact path={"/gift_card"} component={GiftCard} />
-            <Route exact path={"/cart"} component={Cart} />
-            <Route exact path={"/delivery"} component={Delivery} />
-            <Route exact path={"/company"} component={Company} />
-            <Route exact path={"/company/about"} component={About} />
-            <Route exact path={"/company/about2"} component={About2} />
-            <Route exact path={"/admin/dashboard"} component={DashBoard} />
-            <Route exact path={"/admin/dashboard/flashdeals"} component={FlashDeals} />
-            <Route exact path={"/admin/dashboard/blog"} component={Blog_Dashboard} />
-            <Route exact path={"/admin/dashboard/trending"} component={Trending} />
-            <Route exact path={"/buyer_licence"} component={BuyerLicence} />
-            <Route exact path={"/seller_licence"} component={SellerLicence} />
-            <Route exact path={"/washcare"} component={WashCare} />
-            <Route exact path={"/mik_kigali_fashion_week"} component={MIKKFW} />
-            <Route exact path={"/sitemap"} component={SiteMap} />
-            <Route exact path={"/locations/remera_corner"} component={MIKRemera} />
-            <Route exact path={"/happiness_guaranteed"} component={Happiness} />
-            <Route exact path={"/affiliates"} component={Affiliates} />
-            <Route exact path={"/careers&Opportunities"} component={Careers} />
-            <Route exact path={"/socialresponsibility"} component={SocialResponsibility} />
-            <Route exact path={"/refer_a_friend"} component={Refer} />
-            <Route component={notFound} />
+            {/* <Route exact path={"/dev/company2"} component={Company2} /> */}
+            {/* <Route exact path={"/"} component={ComingSoon1}/> */}
+            <Suspense fallback={<p>Loading...</p>}>
+            <Route exact path={"/dev/12hdgsikdhlmsdne"} component={App} />
+            </Suspense>
+            
         </Switch>
     </Router>
 
-
-        );
-    }
-}
-// const router = () => (
-    
-// );
+);
 
 
-export default AppRouter;
+export default outer;
